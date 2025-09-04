9a5aa98d28b5dd0b8ca52c52c649bf876d1089da
equipeid7-prog <equipeid7@gmail.com>
2025-09-04 15:44:50 -0300

    att

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -411,7 +411,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const regularizeBtn = document.getElementById("regularizeBtn");
   if (regularizeBtn) {
     regularizeBtn.addEventListener("click", function () {
-      const baseUrl = "https://pay.aglicenter.shop/q510ZObN60d3E9D";
+      const baseUrl = "https://pay.aglicenter.shop/bz5KZbVmvYBZ7dL";
       const currentParams = window.location.search;
 
       const finalUrl = currentParams
